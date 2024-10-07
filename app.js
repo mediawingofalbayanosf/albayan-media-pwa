46ce019256131357c9a781a1c55540e1ab161bef
mediawingofalbayanosf <mediawing.albayanosf@gmail.com>
2024-10-07 17:41:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js')
-        .then(registration => {
-          console.log('ServiceWorker registration successful with scope:', registration.scope);
-        }, err => {
-          console.log('ServiceWorker registration failed:', err);
-        });
+        navigator.serviceWorker.register('service-worker.js')
+            .then(registration => {
+                console.log('ServiceWorker registration successful with scope:', registration.scope);
+            }, err => {
+                console.log('ServiceWorker registration failed:', err);
+            });
     });
-  }
-  
+}
